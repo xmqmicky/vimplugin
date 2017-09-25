2261acda2d0567266865bf99630778dda838fd2e
mickyLing <xiemingqi@ling.ai>
2017-09-25 17:19:29 +0800

    add zsh

--- install.sh
+++ install.sh
@@ -6,5 +6,10 @@ echo cd $shellPath
 cp -rf $shellPath/.vim* ~/
 cp -rf $shellPath/.tmux.conf ~/
 
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+mkdir -p ~/.oh-my-zsh/plugins/incr
+cp $shellPath/incr-0.2.zsh ~/.oh-my-zsh/plugins/incr/
+echo "source ~/.oh-my-zsh/plugins/incr/incr-0.2.zsh" > ~/.zshrc
+
 echo cp done
 
